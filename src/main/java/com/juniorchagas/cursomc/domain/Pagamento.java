ae5eef865df67913d6c3b5397a543ecda49cfb5f
junior chagas <ivanaldochagas10@gmail.com>
2018-09-24 14:58:01 -0300

    Atualizacao:utilizando somento JsonIngnore

--- src/main/java/com/juniorchagas/cursomc/domain/Pagamento.java
+++ src/main/java/com/juniorchagas/cursomc/domain/Pagamento.java
@@ -3,7 +3,6 @@ package com.juniorchagas.cursomc.domain;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
-
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
@@ -11,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.juniorchagas.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -23,7 +22,7 @@ public abstract class Pagamento implements Serializable{
 	private Integer id;
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
