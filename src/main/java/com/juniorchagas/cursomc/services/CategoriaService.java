19b8983049a54fcbdf96d18b2dcbcb7497a80f6b
junior chagas <ivanaldochagas10@gmail.com>
2018-09-24 16:49:18 -0300

    Deletando uma categoria

--- src/main/java/com/juniorchagas/cursomc/services/CategoriaService.java
+++ src/main/java/com/juniorchagas/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.juniorchagas.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.juniorchagas.cursomc.domain.Categoria;
 import com.juniorchagas.cursomc.repositories.CategoriaRepository;
+import com.juniorchagas.cursomc.services.exceptions.DataIntegrityException;
 import com.juniorchagas.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -33,8 +35,15 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.delete(id);
+	}
+	catch(DataIntegrityViolationException e ) {
+		throw new DataIntegrityException("não é possivel excluir uma categoria que possui produto");
+	}
 	
-	
-	
-	
+	}
 }
+
