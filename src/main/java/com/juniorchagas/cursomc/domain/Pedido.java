651da6c395bc4d3282a8fac9496699b22729dd79
junior chagas <ivanaldochagas10@gmail.com>
2018-09-24 14:48:50 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/juniorchagas/cursomc/domain/Pedido.java
+++ src/main/java/com/juniorchagas/cursomc/domain/Pedido.java
@@ -14,17 +14,23 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 public class Pedido implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern="dd/MM/yyyy hh:mm")
 	private Date instante;
 	
+	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL,mappedBy="pedido")
 	private Pagamento pagamento;
-	
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
---
+++ src/main/java/com/juniorchagas/cursomc/services/PedidoService.java
@@ -0,0 +1,39 @@
+package com.juniorchagas.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juniorchagas.cursomc.domain.Pedido;
+import com.juniorchagas.cursomc.repositories.PedidoRepository;
+import com.juniorchagas.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+
+
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	
+	public Pedido buscar(Integer id) {
+			Pedido obj = repo.findOne(id);
+			if(obj==null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ID:" +id
+					+",Tipo:"+Pedido.class.getName());  	
+			}
+			return obj;
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
