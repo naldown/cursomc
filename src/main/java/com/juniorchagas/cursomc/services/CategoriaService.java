81842acd7f7f87304fad759168b489136767ec15
junior chagas <ivanaldochagas10@gmail.com>
2018-09-24 16:19:51 -0300

    Atualizando Categoeria com PUT, outros pequenos ajustes

--- src/main/java/com/juniorchagas/cursomc/services/CategoriaService.java
+++ src/main/java/com/juniorchagas/cursomc/services/CategoriaService.java
@@ -14,7 +14,7 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 			Categoria obj = repo.findOne(id);
 			if(obj==null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! ID:" +id
@@ -29,6 +29,10 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
 	
 	
 	
--- src/main/java/com/juniorchagas/cursomc/services/PedidoService.java
+++ src/main/java/com/juniorchagas/cursomc/services/PedidoService.java
@@ -18,7 +18,7 @@ public class PedidoService {
 	private PedidoRepository repo;
 	
 	
-	public Pedido buscar(Integer id) {
+	public Pedido find(Integer id) {
 			Pedido obj = repo.findOne(id);
 			if(obj==null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! ID:" +id
